d48957a2a033daf91bd88b9e57227edafc58eaa8
onofflink <42961200+aiegoo@users.noreply.github.com>
2019-04-08 05:16:02 +0900

    add shopping and recipe components with g c cli

--- prj-start/src/app/header/header.components.ts
+++ prj-start/src/app/header/header.components.ts
@@ -1,8 +1,8 @@
 
-import { component } from '@angular/core';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
-  templateUrl:
+  templateUrl:'./header.component.html'
 })
 export class HeaderComponent {
 
